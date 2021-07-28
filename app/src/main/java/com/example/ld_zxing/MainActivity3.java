514f092e0f26d3a571f95f34eda88df690dad780
liu <602694077@qq.com>
2021-07-28 09:17:06 +0800

    2021年7月28日 09:16:33 添加权限验证

--- app/src/main/java/com/example/ld_zxing/MainActivity3.java
+++ app/src/main/java/com/example/ld_zxing/MainActivity3.java
@@ -1,10 +1,13 @@
 package com.example.ld_zxing;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -53,6 +56,7 @@ public class MainActivity3 extends AppCompatActivity {
         }
         // 隐藏状态栏
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main3);
 
@@ -69,12 +73,56 @@ public class MainActivity3 extends AppCompatActivity {
        /* ZxingFragment fragment = new ZxingFragment();
         getSupportFragmentManager().beginTransaction().add(R.id.parent, fragment).commit();*/
 
+        // 获取权限
+        checkPermission();
 
         initView();
 
+
+
+
+    }
+
+    public void checkPermission() {
+        boolean isGranted = true;
+        if (android.os.Build.VERSION.SDK_INT >= 23) {
+            if (this.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                //如果没有写sd卡权限
+                isGranted = false;
+            }
+            if (this.checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                isGranted = false;
+            }
+            Log.i("cbs","isGranted == "+isGranted);
+            if (!isGranted) {
+                this.requestPermissions(
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission
+                                .ACCESS_FINE_LOCATION,
+                                Manifest.permission.READ_EXTERNAL_STORAGE,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                Manifest.permission.CAMERA},
+                        102);
+            }else{
+                initScan();
+            }
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        for (int i = 0; i < grantResults.length; i++) {
+            System.out.println("权限申请 xxxxxxxxxx " + grantResults[i]  + "xxxxxxx " + grantResults.length);
+        }
+        System.out.println("权限申请 bushipiliang xxxxxxxxxx " + requestCode);
+
         initScan();
-
-
+        // System.out.println("xxxxx");
+      /*  if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+        } else {
+        }*/
     }
 
     private void initScan() {
